e0289caf18f07b64e6206861aab51edc154a0c13
Lukasz Kopyto <lukasz.kopyto@gmail.com>
2020-07-17 10:23:45 +0200

    Project handling files

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+var path = require('path')
+var entryPath = 'development'
+var entryFile = 'app.js'
+
+module.exports = {
+  entry: `./${entryPath}/js/${entryFile}`,
+  output: {
+    filename: 'out.js',
+    path: path.resolve(__dirname, `${entryPath}/js`),
+  },
+  mode: 'production', // lub "development"
+  devtool: 'source-map',
+  devServer: {
+    contentBase: path.join(__dirname, entryPath),
+    publicPath: '/js/',
+    compress: false,
+    port: 3001,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+}
