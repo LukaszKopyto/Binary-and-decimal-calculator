c8e2b2c9c7d651a800689f506262e2522f3307c1
Lukasz Kopyto <lukasz.kopyto@gmail.com>
2020-07-17 10:39:01 +0200

    adding an event on the plus sign and completing the addition of decimal numbers

--- development/js/DecCalculator.js
+++ development/js/DecCalculator.js
@@ -5,13 +5,33 @@ class DecCalculator extends Calculator {
     super(settings)
     console.log(this.getName())
   }
-  add() {}
+  add(numberX, numberY) {
+    let result = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    let surplus = 0
+    for (let i = numberX.length - 1; i >= 0; i--) {
+      let outcome = numberX[i] + numberY[i] + surplus
+      surplus = 0
+      if (outcome >= 10) {
+        result[i] = outcome - 10
+        surplus = 1
+      } else {
+        result[i] = outcome
+      }
+    }
+    console.table(result)
+    return result
+  }
   changeNumber() {
     this.$calculatorDOMElement.find('.active').on('click', (e) => {
       $(e.target).empty().attr('contenteditable', 'true').focus()
     })
   }
   updateResult() {}
-  initEvents() {}
+  initEvents() {
+    this.$calculatorDOMElement.find('.operator-bar__add').on('click', (e) => {
+      this.checkNumber()
+      this.updateResult()
+    })
+  }
 }
 export { DecCalculator }
